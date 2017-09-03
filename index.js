ddc4dab5400098d1bd9efbec4e11c93ae9b39f0a
Kyle Murray <kyle_murray@apple.com>
2017-09-03 16:36:41 -0700

    [Status] Fix formatting / linting.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var languageVersions = ['2.2', '3', '3.0.1', '3.1', '4']
 var filterSelection = []
 
 var GITHUB_BASE_URL = 'https://github.com/'
-var REPO_PROPOSALS_BASE_URL =  GITHUB_BASE_URL + 'apple/swift-evolution/blob/master/proposals'
+var REPO_PROPOSALS_BASE_URL = GITHUB_BASE_URL + 'apple/swift-evolution/blob/master/proposals'
 
 /**
  * `name`: Mapping of the states in the proposals JSON to human-readable names.
@@ -847,7 +847,7 @@ function _applyFragment (fragment) {
       })[0]
 
       if (!stateName) return // fragment contains a nonexistent state
-      state = states[stateName]
+      var state = states[stateName]
 
       if (stateName === '.implemented') implementedSelected = true
 
